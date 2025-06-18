b0fbc7c914578ba71281a7d0461799d0b54271da
Lahiru-Bandara <chathuminabandaralahiru@gmail.com>
2025-06-19 02:09:59 +0530

    Your commit message here

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -6,13 +6,11 @@ import org.example.dto.LoginRequestDto;
 import org.example.dto.RegisterRequestDTo;
 import org.example.security.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthController {
 
